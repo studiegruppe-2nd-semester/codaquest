126a51be446a36df26ab88c7e8fadfddb51eba37
DetGrey <anenovlar@gmail.com>
2024-04-17 12:23:26 +0200

    Onboarding layout finished for now

--- app/src/main/java/com/example/codaquest/ui/components/onboarding/OnboardingRadioButtonComposable.kt
+++ app/src/main/java/com/example/codaquest/ui/components/onboarding/OnboardingRadioButtonComposable.kt
@@ -3,6 +3,7 @@ package com.example.codaquest.ui.components.onboarding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.codaquest.ui.theme.CodaQuestTheme
@@ -24,22 +26,25 @@ fun OnboardingRadioButtonComposable (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
+        Text(modifier = Modifier.padding(vertical = 20.dp),
             text = viewModel.questions[viewModel.currentQuestion].question,
             fontSize = 30.sp,
             textAlign = TextAlign.Center,
-            lineHeight = 35.sp
-        )
+            lineHeight = 35.sp,
+            )
 
         viewModel.questions[viewModel.currentQuestion].options?.forEach { option ->
-            Button(
+            Button(modifier = Modifier.padding(vertical = 5.dp),
                 onClick = { viewModel.questions[viewModel.currentQuestion].answer.value = option},
                 colors = ButtonDefaults.buttonColors(
                     containerColor = viewModel.addButtonColor(option),
                     contentColor = Color.Black
                 )
             ) {
-                Text(text = option)
+                Text(
+                    text = option,
+                    fontSize = 20.sp,
+                    )
             }
         }
     }
