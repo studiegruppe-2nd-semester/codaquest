6dd3cda0c0a2040b95385a031f0c311472f0d11c
nathasjafink <nathasja.nielsen@gmail.com>
2024-04-19 13:55:45 +0200

    Styling completed

--- app/src/main/java/com/example/codaquest/ui/components/login/SignUpComposable.kt
+++ app/src/main/java/com/example/codaquest/ui/components/login/SignUpComposable.kt
@@ -1,14 +1,21 @@
 package com.example.codaquest.ui.components.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SignUpComposable(loginViewModel: LoginViewModel) {
-    Column {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         TextField(value = loginViewModel.username,
             onValueChange = { loginViewModel.username },
             label = { Text("Username")})
