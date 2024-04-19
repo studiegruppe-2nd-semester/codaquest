4f7f399d235221d004e5fc191ad752bd2c1fcc0a
kasperhr <kasperhr2000@outlook.dk>
2024-04-19 11:50:15 +0200

    home screen

--- app/src/main/java/com/example/codaquest/ui/components/home/HomeScreen.kt
+++ app/src/main/java/com/example/codaquest/ui/components/home/HomeScreen.kt
@@ -1,11 +1,37 @@
 package com.example.codaquest.ui.components.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.codaquest.ui.components.navbar.NavBar
 
 @Composable
 fun HomeScreen (navController: NavController) {
     // So we can see it the right screen delete the text if necessary
-    Text(text = "HomeScreen")
+
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+
+        Column(modifier = Modifier
+            .padding(5.dp)
+            .fillMaxSize()
+        ) {
+            Text(text = "HomeScreen")
+            // your code starts here
+
+
+
+            // Your code ends here
+        }
+
+        NavBar(navController)
+    }
 }
