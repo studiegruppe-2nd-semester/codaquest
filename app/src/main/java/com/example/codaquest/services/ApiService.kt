ab3b12e5a10d57b7a71cb12b8bc45e07db23787e
DetGrey <anenovlar@gmail.com>
2024-04-24 14:21:35 +0200

    Main merged into api branch to get updates

--- app/src/main/java/com/example/codaquest/services/ApiService.kt
+++ app/src/main/java/com/example/codaquest/services/ApiService.kt
@@ -1,15 +1,14 @@
 package com.example.codaquest.services
 
 import com.aallam.openai.api.chat.ChatCompletion
-import com.aallam.openai.api.chat.ChatCompletionChunk
 import com.aallam.openai.api.chat.ChatCompletionRequest
 import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatResponseFormat
 import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.http.Timeout
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.example.codaquest.ui.components.SharedViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlin.time.Duration.Companion.seconds
 
 class ApiService {
@@ -30,20 +29,28 @@ class ApiService {
     suspend fun promptApi() {
         val chatCompletionRequest = ChatCompletionRequest(
             model = ModelId("gpt-3.5-turbo"),
+            responseFormat = ChatResponseFormat("json_object"),
             messages = listOf(
                 ChatMessage(
                     role = ChatRole.System,
-                    content = "You are a helpful assistant!"
+                    content = "You are a coding project generator. Your job is to come up with interesting projects in a chosen coding " +
+                            "language and to give the user a project title,  project description along with project requirements (which can also be " +
+                            "seen as steps to complete the project)"
                 ),
                 ChatMessage(
                     role = ChatRole.User,
-                    content = "Hello!"
+                    content = "I am looking for a project that fits these key words: \"view models, basic android app\". The project should be an" +
+                            "app made by Kotlin Jetpack Compose, and the average completion time for the project should be 5 hours. I am a beginner" +
+                            "in coding with Kotlin"
                 )
             )
         )
         val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
+        // HERE YOU ONLY GET THE MESSAGE CONTENT THAT THE AI ANSWERED
         println("COMPLETION: $completion")
+        println("COMPLETION: ${completion.choices[0].message.content}")
+
         // or, as flow
-        val completions: Flow<ChatCompletionChunk> = openAI.chatCompletions(chatCompletionRequest)
+//        val completions: Flow<ChatCompletionChunk> = openAI.chatCompletions(chatCompletionRequest)
     }
 }
