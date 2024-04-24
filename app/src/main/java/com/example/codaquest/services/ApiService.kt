60aa6362fac4dfa64da5bc99474fcf54435bb165
nathasjafink <nathasja.nielsen@gmail.com>
2024-04-24 14:42:55 +0200

    added prompt and connected promptApi to home viewmodel

--- app/src/main/java/com/example/codaquest/services/ApiService.kt
+++ app/src/main/java/com/example/codaquest/services/ApiService.kt
@@ -8,7 +8,9 @@ import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.http.Timeout
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
+import com.example.codaquest.classes.Project
 import com.example.codaquest.ui.components.SharedViewModel
+import com.example.codaquest.ui.components.home.HomeScreenViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlin.time.Duration.Companion.seconds
 
@@ -27,17 +29,19 @@ class ApiService {
         }!!
     }
 
-    suspend fun promptApi() {
+    suspend fun promptApi(homeScreenViewModel: HomeScreenViewModel) {
         val chatCompletionRequest = ChatCompletionRequest(
             model = ModelId("gpt-3.5-turbo"),
             messages = listOf(
                 ChatMessage(
                     role = ChatRole.System,
-                    content = "You are a helpful assistant!"
+                    content = "You are a coding project generator. Your job is to come up with interesting projects in a chosen coding language and to give the user a project title,  project description along with steps to guide the user through the project, please make detailed steps. The project could be an app or a website, you will decide whats possible to make with the given coding language. So e.g JavaScript is typical used for website and Kotlin is typical used for apps, and if it's frontend, backend or fullstack the keywords match best with. \n" +
+                            "\n" +
+                            "I want the response formatted as a JSON file with title (string), description (string), steps (list of string), level (int), language (string), length (int)"
                 ),
                 ChatMessage(
                     role = ChatRole.User,
-                    content = "Hello!"
+                    content = "I'm ${homeScreenViewModel.level} with the coding language ${homeScreenViewModel.language} and i am looking for a project that fits these key words: ${homeScreenViewModel.keyWords}. The average completion time for the project should be ${homeScreenViewModel.length} hours. "
                 )
             )
         )
