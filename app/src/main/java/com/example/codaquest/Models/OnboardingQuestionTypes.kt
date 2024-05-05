84037d57cdf582747f907f38a3927ddfa3ce44e3
DetGrey <anenovlar@gmail.com>
2024-05-05 15:22:13 +0200

    models package problem

--- app/src/main/java/com/example/codaquest/Models/OnboardingQuestionTypes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.codaquest.Models
-
-enum class OnboardingQuestionTypes {
-    RadioButton,
-    TextField,
-    IntField
-}
--- app/src/main/java/com/example/codaquest/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.codaquest.models
-
-data class User(
-    val userUid: String,
-    val username: String?,
-    var onboardingData: OnboardingData? = null,
-    var projects: MutableList<Project>? = null,
-)
