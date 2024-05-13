53c3db4ec23e21208b66d32739cd66c65c5a0b88
nathasjafink <nathasja.nielsen@gmail.com>
2024-05-13 20:53:35 +0200

    added styling to the created elements

--- app/src/main/java/com/example/codaquest/ui/components/settings/SettingScreen.kt
+++ app/src/main/java/com/example/codaquest/ui/components/settings/SettingScreen.kt
@@ -1,11 +1,15 @@
 package com.example.codaquest.ui.components.settings
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,21 +27,33 @@ fun SettingsScreen(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(10.dp),
     ) {
-        Row {
+        Row(modifier = Modifier,
+            horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_back_arrow),
                 contentDescription = "back arrow icon",
-                modifier = Modifier.size(100.dp),
+                modifier = Modifier.size(70.dp),
             )
+            Spacer(modifier = Modifier.width(60.dp))
+            Text(
+                text = "Setting",
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .padding(top = 15.dp)
+            )
+
         }
 
-        Box {
+        Box(modifier = Modifier.padding(top = 15.dp)) {
             Text(
                 text = "Account setting",
                 fontSize = 25.sp,
             )
+
         }
     }
 }
