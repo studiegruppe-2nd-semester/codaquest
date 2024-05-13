79d9635c7b10af1a0e3d14b1b16ffad485f29f6e
nathasjafink <nathasja.nielsen@gmail.com>
2024-05-13 11:51:25 +0200

    added a back arrow and a text

--- app/src/main/java/com/example/codaquest/ui/components/settings/SettingScreen.kt
+++ app/src/main/java/com/example/codaquest/ui/components/settings/SettingScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,36 +13,33 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.codaquest.ui.components.SharedViewModel
 import com.example.codaquest.R
 
 @Composable
-fun SettingsScreen (
-
-) {
-    Column (modifier = Modifier
-        .fillMaxSize()
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
     ) {
         Row {
             Image(
                 painter = painterResource(id = R.drawable.ic_back_arrow),
                 contentDescription = "back arrow icon",
-                modifier = Modifier.size(100.dp))
-        }
-
-
-        Box {
-            Text(text = "Account setting",
-                fontSize = 25.sp
+                modifier = Modifier.size(100.dp),
             )
         }
 
+        Box {
+            Text(
+                text = "Account setting",
+                fontSize = 25.sp,
+            )
+        }
     }
 }
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun SettingsScreenPreview (){
+fun SettingsScreenPreview() {
     SettingsScreen()
 }
