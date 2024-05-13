85ace5f8d9cd0ca33c4b63a3d0b1f060599df369
DetGrey <anenovlar@gmail.com>
2024-05-13 12:51:35 +0200

    Login and signup validation tests (unit tests) created

---
+++ app/src/main/java/com/example/codaquest/util/UserValidationUtil.kt
@@ -0,0 +1,80 @@
+package com.example.codaquest.util
+
+import com.example.codaquest.domain.models.LoginInfo
+
+object UserValidationUtil {
+    // ------------------------------------------- VALIDATE LOGIN
+    fun validateLogin(
+        loginInfo: LoginInfo,
+    ): ValidationResult {
+        // ------------------------------------- EMAIL
+        val emailValidationResult: ValidationResult = emailValidation(loginInfo.email)
+
+        if(emailValidationResult is ValidationResult.Error) {
+            return ValidationResult.Error(emailValidationResult.message)
+        }
+
+        // ------------------------------------- PASSWORD
+        val passwordValidationResult: ValidationResult = passwordValidation(loginInfo.password)
+
+        if(passwordValidationResult is ValidationResult.Error) {
+            return ValidationResult.Error(passwordValidationResult.message)
+        }
+
+        return ValidationResult.Success
+    }
+    // ------------------------------------------- VALIDATE SIGNUP
+    fun validateSignUp(
+        loginInfo: LoginInfo,
+    ): ValidationResult {
+        // ------------------------------------- USERNAME
+        if (loginInfo.username.isEmpty()) {
+            return ValidationResult.Error("Username cannot be empty")
+        }
+
+        // ------------------------------------- EMAIL
+        val emailValidationResult: ValidationResult = emailValidation(loginInfo.email)
+
+        if (emailValidationResult is ValidationResult.Error) {
+            return ValidationResult.Error(emailValidationResult.message)
+        }
+
+        // ------------------------------------- PASSWORD
+        if (loginInfo.password != loginInfo.confirmPassword) {
+            return ValidationResult.Error("Confirmed password does not match")
+        }
+
+        val passwordValidationResult: ValidationResult = passwordValidation(loginInfo.password)
+
+        if(passwordValidationResult is ValidationResult.Error) {
+            return ValidationResult.Error(passwordValidationResult.message)
+        }
+
+        return ValidationResult.Success
+    }
+
+    private fun emailValidation(email: String): ValidationResult {
+        if (email.isEmpty()) {
+            return ValidationResult.Error("Email cannot be empty")
+        }
+
+        val emailRegex = "^([A-Za-z0-9._%+-])+@[a-zA-Z0-9-]+\\.[a-zA-Z.]{2,}\$"
+        if (!email.matches(emailRegex.toRegex())) {
+            return ValidationResult.Error("Wrong email format")
+        }
+
+        return ValidationResult.Success
+    }
+
+    private fun passwordValidation(password: String): ValidationResult {
+        if (password.isEmpty()) {
+            return ValidationResult.Error("Password cannot be empty")
+        }
+
+        if (password.length < 6) {
+            return ValidationResult.Error("Password must have at least 6 characters")
+        }
+
+        return ValidationResult.Success
+    }
+}
---
+++ app/src/test/java/com/example/codaquest/UserValidationUtilTest.kt
@@ -0,0 +1,111 @@
+package com.example.codaquest
+
+import com.example.codaquest.domain.models.LoginInfo
+import com.example.codaquest.util.UserValidationUtil
+import com.example.codaquest.util.ValidationResult
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UserValidationUtilTest {
+    // ------------------------------------- EMAIL
+    @Test
+    fun `empty username returns error`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "",
+                password = "password",
+            ),
+        )
+        assertTrue(result is ValidationResult.Error)
+
+        // If you want to check the error message, you can do:
+//        val errorMessage = (result as ValidationResult.Error).message
+//        assertEquals("Email cannot be empty", errorMessage)
+    }
+
+    @Test
+    fun `wrong email format returns error`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "@email.dk",
+                password = "password",
+            ),
+        )
+//        assertTrue(result is ValidationResult.Error)
+        assertEquals("Wrong email format", (result as ValidationResult.Error).message)
+    }
+    @Test
+    fun `wrong email format returns error 2`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "hi@.dk",
+                password = "password",
+            ),
+        )
+        assertEquals("Wrong email format", (result as ValidationResult.Error).message)
+    }
+    @Test
+    fun `wrong email format returns error 3`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "hi@email",
+                password = "password",
+            ),
+        )
+//        assertTrue(result is ValidationResult.Error)
+        assertEquals("Wrong email format", (result as ValidationResult.Error).message)
+    }
+    @Test
+    fun `wrong email format returns error 4`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "hi@email.2com",
+                password = "password",
+            ),
+        )
+//        assertTrue(result is ValidationResult.Error)
+        assertEquals("Wrong email format", (result as ValidationResult.Error).message)
+    }
+    @Test
+    fun `valid email format returns success`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "email@email.com",
+                password = "password",
+            ),
+        )
+        assertTrue(result is ValidationResult.Success)
+    }
+    // ------------------------------------- PASSWORD
+    @Test
+    fun `empty password returns error`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "email@email.com",
+                password = "",
+            ),
+        )
+        assertTrue(result is ValidationResult.Error)
+    }
+    @Test
+    fun `too short password returns error`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "email@email.com",
+                password = "pass1",
+            ),
+        )
+        assertTrue(result is ValidationResult.Error)
+    }
+    @Test
+    fun `valid password returns success`() {
+        val result = UserValidationUtil.validateLogin(
+            LoginInfo(
+                email = "email@email.com",
+                password = "password",
+            ),
+        )
+        assertTrue(result is ValidationResult.Success)
+    }
+}
