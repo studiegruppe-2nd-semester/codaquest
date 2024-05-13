80191fc39d41843095d61f57ccbb51b4dc4964d6
a1-vincent <aaronnavarro26@gmail.com>
2024-05-13 11:07:54 +0200

    Confirm log out button in ProfileScreen.kt

--- app/src/main/java/com/example/codaquest/ui/components/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/codaquest/ui/components/profile/ProfileScreen.kt
@@ -13,11 +13,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,6 +42,7 @@ fun ProfileScreen(
     sharedViewModel: SharedViewModel,
 ) {
     val profileViewModel: ProfileViewModel = viewModel()
+    var showDialog by remember { mutableStateOf(false) }
 
     sharedViewModel.user?.onboardingData?.let {
         addOnboardingDataToProfileViewModel(
@@ -65,21 +71,54 @@ fun ProfileScreen(
                         contentDescription = "settings icon",
                     )
 
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_logout),
-                        contentDescription = "settings icon",
-                        modifier = Modifier
-                            .clickable {
-                                profileViewModel.logout(
-                                    onSuccess = {
-                                        sharedViewModel.changeUser(null)
-                                        sharedViewModel.generatedProject = Project()
-                                        navController.navigate("home")
+                    Spacer(modifier = Modifier.height(50.dp))
+
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_logout),
+                            contentDescription = "logout icon",
+                            modifier = Modifier
+                                .clickable {
+                                    profileViewModel.logout(
+                                        onSuccess = {
+                                            showDialog = true
+                                        },
+                                    )
+                                },
+                        )
+                    }
+                    if (showDialog) {
+                        AlertDialog(
+                            onDismissRequest = { showDialog = false },
+                            title = { Text("Logout") },
+                            text = { Text("Are you sure you want to log out?") },
+                            confirmButton = {
+                                Button(
+                                    onClick = {
+                                        profileViewModel.logout(
+                                            onSuccess = {
+                                                sharedViewModel.changeUser(null)
+                                                sharedViewModel.generatedProject = Project()
+                                                navController.navigate("home")
+                                                showDialog = false
+                                            },
+                                        )
                                     },
-                                )
+                                ) {
+                                    Text("Logout")
+                                }
                             },
-                    )
+                            dismissButton = {
+                                Button(
+                                    onClick = { showDialog = false },
+                                ) {
+                                    Text("Cancel")
+                                }
+                            }
+                        )
+                    }
                 }
+
 
                 Column(
                     modifier = Modifier
@@ -181,6 +220,7 @@ fun ProfileScreen(
         NavBar("profile", navController, sharedViewModel)
     }
 }
+
 
 fun findOnboardingData(name: String, onboardingData: OnboardingData?): String {
     return when (name) {
