73b080d5691eeb4244070a9c94995efb0c3b56f4
nathasjafink <nathasja.nielsen@gmail.com>
2024-05-19 15:37:03 +0200

    removed = null

--- app/src/main/java/com/example/codaquest/domain/models/User.kt
+++ app/src/main/java/com/example/codaquest/domain/models/User.kt
@@ -3,7 +3,7 @@ package com.example.codaquest.domain.models
 data class User(
     val userUid: String,
     val username: String?,
-    val email: String? = null,
+    val email: String?,
     var onboardingData: OnboardingData? = null,
     var projects: MutableList<Project>? = null,
 )
