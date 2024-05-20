feed41a550c0ab4f1ea1fb4401bf814182e60b76
DetGrey <anenovlar@gmail.com>
2024-05-20 11:15:40 +0200

    Removed commented out variables in Theme.kt etc

--- app/src/main/java/com/example/codaquest/ui/theme/Type.kt
+++ app/src/main/java/com/example/codaquest/ui/theme/Type.kt
@@ -34,14 +34,4 @@ val Typography = Typography(
         textAlign = TextAlign.Center,
         color = Color.White,
     ),
-/*
-labelSmall = TextStyle(
-fontFamily = FontFamily.Default,
-fontWeight = FontWeight.Medium,
-fontSize = 11.sp,
-lineHeight = 16.sp,
-letterSpacing = 0.5.sp
-)
-*/
-
 )
